254d3b3a1892479a75b4cb9a511394e8413b586e
Frank Ellis <frank.r.ellis@gmail.com>
2026-04-17 23:50:06 -0700

    ggml-cpu: add TQ3_1S, TQ4_1S, and turbo KV cache types to clamp switch

--- ggml/src/ggml-cpu/ops.cpp
+++ ggml/src/ggml-cpu/ops.cpp
@@ -5591,7 +5591,12 @@ void ggml_compute_forward_clamp(
         case GGML_TYPE_I16:
         case GGML_TYPE_I32:
         case GGML_TYPE_I64:
-        case GGML_TYPE_F64:
+         case GGML_TYPE_F64:
+        case GGML_TYPE_TQ3_1S:
+        case GGML_TYPE_TQ4_1S:
+        case GGML_TYPE_TURBO3_0:
+        case GGML_TYPE_TURBO4_0:
+        case GGML_TYPE_TURBO2_0:
         case GGML_TYPE_COUNT:
             {
                 GGML_ABORT("fatal error");
